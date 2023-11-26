34e40c77c48b26fbd780e735f85fcea3d7824048
Ujjawal Saini <sainiujjawal4239@gmail.com>
2023-11-26 23:50:10 +0530

    ImprovedPerformence and camera movement script

--- Assets/Game/Scripts/Camera/CameraMovement.cs
+++ Assets/Game/Scripts/Camera/CameraMovement.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private float minY, maxY;
     [SerializeField] private float smoothTime = 0.125f;
+    [SerializeField] private float upTime = 0.5f;
+    [SerializeField] private float downTime = 0.5f;
+
     [SerializeField] private VoidEventChannelSO onCountDownFinished;
     [SerializeField] private GameObject water;
-
+    
+    
     private Vector3 _velocity;
 
     private void OnEnable() => onCountDownFinished.RegisterListener(MoveCameraToTheTop);
@@ -23,11 +28,36 @@ public class CameraMovement : MonoBehaviour
     
     private void MoveCameraToTheTop()
     {
-        Vector3 targetPosition = transform.position;
-        targetPosition.y = maxY;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
-        water.SetActive(true);
+        Vector3 targetPosition = new Vector3(transform.position.x, maxY, transform.position.z);
+        StartCoroutine(SmoothMoveToTop(targetPosition, upTime));
     }
+
+    private IEnumerator SmoothMoveToTop(Vector3 targetPosition, float time)
+    {
+        float elapsedTime = 0f;
+        Vector3 initialPosition = transform.position;
+
+        while (elapsedTime < time)
+        {
+            transform.position = Vector3.Lerp(initialPosition, targetPosition, elapsedTime / time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        
+        
+        //when camera reaches the top, it will activate the water particle system
+        water.SetActive(true);
+        Invoke(nameof(MoveCameraToTheBottom), 1.5f);
+    }
+    
+    private void MoveCameraToTheBottom()
+    {
+        Vector3 targetPosition = new Vector3(transform.position.x, minY, transform.position.z);
+        StartCoroutine(SmoothMoveToTop(targetPosition, downTime));
+    }
+
 
     private void OnDisable() => onCountDownFinished.UnregisterListener(MoveCameraToTheTop);
 }
---
+++ Assets/Game/Scripts/GameObjects/DestroyWaterParticle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DestroyWaterParticle : MonoBehaviour
+{
+    [SerializeField] private float minYDistance = 500f;    
+    void Update()
+    {
+        foreach (Transform child in transform)                   //FOR PERFORMANCE REASONS, IT WILL DELETE THE PARTICLES THAT ARE TOO FAR AWAY FROM THE CAMERA
+        {
+            if (child.position.y < minYDistance)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
