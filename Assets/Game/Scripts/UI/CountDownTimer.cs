0338a88a83c899ac1980d8ab727ec6d850191bc1
Ujjawal Saini <sainiujjawal4239@gmail.com>
2023-11-27 18:12:52 +0530

    Added Bucket Logic and count down timer

---
+++ Assets/Game/Scripts/UI/CountDownTimer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class CountDownTimer : MonoBehaviour
+{
+    [SerializeField] private float maxTime = 5;
+    [SerializeField] private VoidEventChannelSO startSimulation;
+    [SerializeField] private VoidEventChannelSO levelFinished;
+
+    private TextMeshProUGUI uiText;
+
+    private void Awake()
+    {
+        startSimulation.RegisterListener(StartCounting);
+        uiText = GetComponent<TextMeshProUGUI>();
+        uiText.text = null;
+    }
+    
+    private void StartCounting()
+    {
+        StartCoroutine(GameTimer());
+    }
+    
+    IEnumerator GameTimer()
+    {
+        float currentTime = maxTime;
+        while (currentTime >=0)
+        {
+            uiText.text = Mathf.RoundToInt(currentTime).ToString();
+            currentTime -= Time.deltaTime;
+            yield return null;
+        }
+        
+        levelFinished.RaiseEvent();
+    }
+    
+    private void OnDisable()
+    {
+        startSimulation.UnregisterListener(StartCounting);
+    }
+}
--- Assets/Game/Scripts/UI/GameOverUI.cs
+++ Assets/Game/Scripts/UI/GameOverUI.cs
@@ -1,8 +1,27 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
+    [SerializeField] private VoidEventChannelSO levelFinished;
+    [SerializeField] private Bucket bucket;
+    [SerializeField] private TextMeshProUGUI gameOverText;
+
+    private void Awake()
+    {
+        levelFinished.RegisterListener(OnAllChildren);
+    }
+
+    private void OnAllChildren()
+    {
+        gameOverText.text = bucket.waterAmount >= 50 ? "Bucket Filled !" : "Game Over !";
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
     public void OnClickReplyButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -11,5 +30,10 @@ public class GameOverUI : MonoBehaviour
     public void OnClickNextLevelButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    private void OnDisable()
+    {
+        levelFinished.UnregisterListener(OnAllChildren);
     }
 }
